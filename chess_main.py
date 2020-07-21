c9c63b28c3fe10474e44bc739185853152c4b46d
Christoffer Backlund <c.backlund@hotmail.com>
2020-07-21 23:19:26 +0200

    Changed main to pygame instead of tkinter

--- chess_main.py
+++ chess_main.py
@@ -1,25 +1,18 @@
-import tkinter as tk
+import pygame
+import sys
 
-root = tk.Tk()
+pygame.init()
 
-height = 800
-width = 800
-eightW = width / 8
-eightH = height / 8
+size = hight, width = 800, 800
 
-canvas = tk.Canvas(root, height=height, width=width)
+surface = pygame.display.set_mode(size)
 
-fill = True
-for i in range(8):
-    fill = not fill
-    for j in range(8):
-        if fill:                
-            rec = canvas.create_rectangle(eightW * j, eightH *i, eightW * (j+1), eightH * (i+1), fill="black") 
-        else:
-            rec = canvas.create_rectangle(eightW * j, eightH *i, eightW * (j+1), eightH * (i+1))
-        fill = not fill
+playing = True
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
 
-canvas.create_oval(width / 32, height / 32 * 29, width / 32 * 3, height / 32 * 31, fill="blue")
+    
+    surface.fill((0, 0, 0))
 
-canvas.pack()
-root.mainloop()
+    pygame.display.flip()
