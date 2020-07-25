8958b09eed4a278b82e7f3d258663eac75a84458
Christoffer Backlund <c.backlund@hotmail.com>
2020-07-25 17:46:25 +0200

    Addid Checking logic

--- chess_board.py
+++ chess_board.py
@@ -1,5 +1,6 @@
 import pygame
 from chess_pieces import *
+from copy import deepcopy
 
 class Board:
     def __init__(self):
@@ -12,45 +13,34 @@ class Board:
     def reset_board_moves(self):
         self.chosen_piece = (None, None)
         self.board_moves = [[None for x in range(8)] for y in range(8)]
+        self.update_all_moves(self.board)
+
+    def update_all_moves(self, board):
+        for i in range(8):
+            for j in range(8):
+                if board[i][j]:
+                    board[i][j].update_moves(board)
 
     def initiate_board(self):
-        self.board[7][3] = Queen_w()
-        self.board[7][3].pos = (3, 7)
-        self.board[0][3] = Queen_b()
-        self.board[0][3].pos = (3, 0)
-        self.board[7][4] = King_w()
-        self.board[7][4].pos = (4, 7)
-        self.board[0][4] = King_b()
-        self.board[0][4].pos = (4, 0)
-        self.board[7][2] = Bishop_w()
-        self.board[7][2].pos = (2, 7)
-        self.board[0][2] = Bishop_b()
-        self.board[0][2].pos = (2, 0)
-        self.board[7][5] = Bishop_w()
-        self.board[7][5].pos = (5, 7)
-        self.board[0][5] = Bishop_b()
-        self.board[0][5].pos = (5, 0)
-        self.board[7][1] = Knight_w()
-        self.board[7][1].pos = (1, 7)
-        self.board[0][1] = Knight_b()
-        self.board[0][1].pos = (1, 0)
-        self.board[7][6] = Knight_w()
-        self.board[7][6].pos = (6, 7)
-        self.board[0][6] = Knight_b()
-        self.board[0][6].pos = (6, 0)
-        self.board[7][0] = Rook_w()
-        self.board[7][0].pos = (0, 7)
-        self.board[0][0] = Rook_b()
-        self.board[0][0].pos = (0, 0)
-        self.board[7][7] = Rook_w()
-        self.board[7][7].pos = (7, 7)
-        self.board[0][7] = Rook_b()
-        self.board[0][7].pos = (7, 0)
+        self.board[7][3] = Queen_w(3, 7)
+        self.board[0][3] = Queen_b(3, 0)
+        self.board[7][4] = King_w(4, 7)
+        self.board[0][4] = King_b(4, 0)
+        self.board[7][2] = Bishop_w(2, 7)
+        self.board[0][2] = Bishop_b(2, 0)
+        self.board[7][5] = Bishop_w(5, 7)
+        self.board[0][5] = Bishop_b(5, 0)
+        self.board[7][1] = Knight_w(1, 7)
+        self.board[0][1] = Knight_b(1, 0)
+        self.board[7][6] = Knight_w(6, 7)
+        self.board[0][6] = Knight_b(6, 0)
+        self.board[7][0] = Rook_w(0, 7)
+        self.board[0][0] = Rook_b(0, 0)
+        self.board[7][7] = Rook_w(7, 7)
+        self.board[0][7] = Rook_b(7, 0)
         for i in range(8):
-            self.board[1][i] = Pawn_b()
-            self.board[1][i].pos = (i, 1)
-            self.board[6][i] = Pawn_w()
-            self.board[6][i].pos = (i, 6)
+            self.board[1][i] = Pawn_b(i, 1)
+            self.board[6][i] = Pawn_w(i, 6)
 
 
     def draw(self, surface):
@@ -68,21 +58,70 @@ class Board:
                 if self.board[i][j]:
                     surface.blit(self.board[i][j].img, (j*self.square_size, i*self.square_size))
 
+    def get_oponents_moves(self, board):
+        moves = []
+        for i in range(8):
+            for j in range(8):
+                if board[i][j]:
+                    if board[i][j].isWhite != self.whites_turn:
+                        moves += board[i][j].get_moves()
+        return moves
+
+    def locate_king(self, board):
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] and board[i][j].isKing and board[i][j].isWhite == self.whites_turn:
+                    return (j, i)
+
+    def isCheck(self, board):
+        position_king = self.locate_king(board)
+        all_oponent_moves = self.get_oponents_moves(board)
+        if position_king in all_oponent_moves:
+            return True
+
+    def reverse_move(self, move, saved_square):
+            self.board[self.chosen_piece[1]][self.chosen_piece[0]] = self.board[move[1]][move[1]] 
+            self.board[self.chosen_piece[1]][self.chosen_piece[0]].pos = self.chosen_piece
+            self.board[move[1]][move[1]] = saved_square 
+            self.update_all_moves(self.board)
+
+    def update_legal_moves(self, moves):
+        legal_moves = []
+        for move in moves:
+            if move == self.chosen_piece:
+                legal_moves.append(move)
+                continue
+            saved_square = self.board[move[1]][move[1]]
+            self.board[move[1]][move[1]] = self.board[self.chosen_piece[1]][self.chosen_piece[0]]
+            self.board[self.chosen_piece[1]][self.chosen_piece[0]] = None
+            self.board[move[1]][move[1]].pos = move 
+            self.update_all_moves(self.board)
+            if self.isCheck(self.board):
+                self.reverse_move(move, saved_square)
+                continue
+            else:
+                self.reverse_move(move, saved_square)
+                legal_moves.append(move)
+        return legal_moves
+
 
     def get_mouse(self, x, y, surface):
         x = x // self.square_size
         y = y // self.square_size
-        
         if self.board_moves[y][x]: 
+            print(self.board[y][x])
             if self.board[y][x]:
+                print(self.board[y][x].pos)
+                print(self.chosen_piece)
                 if self.board[y][x].pos == self.chosen_piece:
                     return
             self.board[y][x] = self.board[self.chosen_piece[1]][self.chosen_piece[0]]
             self.board[self.chosen_piece[1]][self.chosen_piece[0]] = None
-            self.reset_board_moves()
             self.board[y][x].pos = (x, y)
             self.board[y][x].first_move = False
             self.whites_turn = not self.whites_turn
+            self.reset_board_moves()
+            self.isCheck(self.board)
             return
         else:
             self.reset_board_moves()
@@ -90,6 +129,7 @@ class Board:
             if self.board[y][x].isWhite != self.whites_turn:
                 return
             self.chosen_piece = (x, y)
-            moves = self.board[y][x].get_moves(self.board)
+            moves = self.board[y][x].get_moves()
+            moves = self.update_legal_moves(moves)
             for move in moves:
                 self.board_moves[move[1]][move[0]] = True
--- chess_pieces.py
+++ chess_pieces.py
@@ -4,11 +4,12 @@ from collections import namedtuple
 
 
 class Pieces:
-    def __init__(self):
-        position = namedtuple('Position', ['x', 'y'])
-        self.pos = position(None, None)
+    def __init__(self, x, y):
+        self.pos = (x, y) 
         self.square_size = 95
         self.first_move = True
+        self.isKing = False
+        self.possible_moves = [self.pos]
 
     def add_pos(self, xs, ys):
         pos = tuple(x + y for x, y in zip(xs, ys))
@@ -18,8 +19,8 @@ class Pieces:
             return None
         return pos
 
-    def get_moves(self, board):
-        return [self.pos]
+    def get_moves(self):
+        return self.possible_moves
 
     def check_recursive(self, pos, dir, board):
         new_pos = self.add_pos(pos, dir)
@@ -34,242 +35,232 @@ class Pieces:
             moves = [new_pos] + self.check_recursive(new_pos, dir, board)
         return moves
 
+    def __repr__(self):
+        return self.symbol
+
 class Queen_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
-        self.pos = (3, 7)
         self.symbol = "Q"
         self.img = pygame.image.load(os.path.join('images', 'w_queen_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1), (1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 class Queen_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
         self.symbol = "q"
         self.img = pygame.image.load(os.path.join('images', 'b_queen_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1), (1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 class King_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
+        self.isKing = True
         self.symbol = "K"
         self.img = pygame.image.load(os.path.join('images', 'w_king_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1), (1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for d in self.directions:
-            new_cap = self.add_pos(self.pos, d)
-            if new_cap:
-                if board[new_cap[1]][new_cap[0]]:
-                    if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+            new_pos = self.add_pos(self.pos, d)
+            if new_pos:
+                if board[new_pos[1]][new_pos[0]]:
+                    if board[new_pos[1]][new_pos[0]].isWhite != self.isWhite:
+                        self.possible_moves.append(new_pos)
                 else:
-                    moves.append(new_cap)
-        return moves
+                    self.possible_moves.append(new_pos)
 
 class King_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
+        self.isKing = True
         self.symbol = "k"
         self.img = pygame.image.load(os.path.join('images', 'b_king_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1), (1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for d in self.directions:
-            new_cap = self.add_pos(self.pos, d)
-            if new_cap:
-                if board[new_cap[1]][new_cap[0]]:
-                    if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+            new_pos = self.add_pos(self.pos, d)
+            if new_pos:
+                if board[new_pos[1]][new_pos[0]]:
+                    if board[new_pos[1]][new_pos[0]].isWhite != self.isWhite:
+                        self.possible_moves.append(new_pos)
                 else:
-                    moves.append(new_cap)
-        return moves
+                    self.possible_moves.append(new_pos)
 
 class Bishop_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
         self.symbol = "B"
         self.img = pygame.image.load(os.path.join('images', 'w_bishop_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 class Bishop_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
         self.symbol = "b"
         self.img = pygame.image.load(os.path.join('images', 'b_bishop_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-    
-    def get_moves(self, board):
         self.directions = [(1, 1), (-1, -1), (1, -1), (-1 , 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 class Knight_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
         self.symbol = "KN"
         self.img = pygame.image.load(os.path.join('images', 'w_knight_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
+        self.directions = [(2, 1), (2, -1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (1, -2), (-1, -2)]
 
-    def get_moves(self, board):
-        #TODO check why (-2, -1) does not seem to work
-        self.directions = [(2, 1), (2, -1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (1, -2), (-1, 2)]
-        moves = [self.pos]
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for d in self.directions:
-            new_cap = self.add_pos(self.pos, d)
-            if new_cap:
-                if board[new_cap[1]][new_cap[0]]:
-                    if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+            new_pos = self.add_pos(self.pos, d)
+            if new_pos:
+                if board[new_pos[1]][new_pos[0]]:
+                    if board[new_pos[1]][new_pos[0]].isWhite != self.isWhite:
+                        self.possible_moves.append(new_pos)
                 else:
-                    moves.append(new_cap)
-        return moves
+                    self.possible_moves.append(new_pos)
 
 class Knight_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
         self.symbol = "kn"
         self.img = pygame.image.load(os.path.join('images', 'b_knight_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-        
-    def get_moves(self, board):
         self.directions = [(2, 1), (2, -1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (1, -2), (-1, 2)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for d in self.directions:
-            new_cap = self.add_pos(self.pos, d)
-            if new_cap:
-                if board[new_cap[1]][new_cap[0]]:
-                    if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+            new_pos = self.add_pos(self.pos, d)
+            if new_pos:
+                if board[new_pos[1]][new_pos[0]]:
+                    if board[new_pos[1]][new_pos[0]].isWhite != self.isWhite:
+                        self.possible_moves.append(new_pos)
                 else:
-                    moves.append(new_cap)
-        return moves
+                    self.possible_moves.append(new_pos)
 
 class Rook_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
         self.symbol = "R"
         self.img = pygame.image.load(os.path.join('images', 'w_rook_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 class Rook_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
         self.symbol = "r"
         self.img = pygame.image.load(os.path.join('images', 'b_rook_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-        
-    def get_moves(self, board):
         self.directions = [(1, 0), (-1, 0), (0, 1), (0 , -1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for direction in self.directions:
-            moves += self.check_recursive(self.pos, direction, board)
-        return moves
+            self.possible_moves += self.check_recursive(self.pos, direction, board)
 
 
 class Pawn_w(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = True
         self.symbol = "P"
         self.img = pygame.image.load(os.path.join('images', 'w_pawn_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-    
-    def get_moves(self, board):
         self.move = (0, -1)
         self.caps = [(-1, -1), (1, -1)]
-        moves = [self.pos]
+    
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for cap in self.caps:
             new_cap = self.add_pos(self.pos, cap)
             if new_cap:
                 if board[new_cap[1]][new_cap[0]]:
                     if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+                        self.possible_moves.append(new_cap)
         new_pos = self.add_pos(self.pos, self.move)
         if board[new_pos[1]][new_pos[0]]:
-            return moves
+            return 
         else:
-            moves.append(new_pos)
+            self.possible_moves.append(new_pos)
         if self.first_move:
             new_pos = self.add_pos(new_pos, self.move)
             if board[new_pos[1]][new_pos[0]]:
-                return moves
+                return 
             else:
-                moves.append(new_pos)
-        return moves
-
+                self.possible_moves.append(new_pos)
         
 class Pawn_b(Pieces):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.isWhite = False
         self.symbol = "p"
         self.img = pygame.image.load(os.path.join('images', 'b_pawn_png_512px.png'))
         self.img = pygame.transform.scale(self.img, (self.square_size, self.square_size))
-
-    def get_moves(self, board):
         self.move = (0, 1)
         self.caps = [(-1, 1), (1, 1)]
-        moves = [self.pos]
+
+    def update_moves(self, board):
+        self.possible_moves = [self.pos]
         for cap in self.caps:
             new_cap = self.add_pos(self.pos, cap)
             if new_cap:
                 if board[new_cap[1]][new_cap[0]]:
                     if board[new_cap[1]][new_cap[0]].isWhite != self.isWhite:
-                        moves.append(new_cap)
+                        self.possible_moves.append(new_cap)
         new_pos = self.add_pos(self.pos, self.move)
         if board[new_pos[1]][new_pos[0]]:
-            return moves
+            return 
         else:
-            moves.append(new_pos)
+            self.possible_moves.append(new_pos)
         if self.first_move:
             new_pos = self.add_pos(new_pos, self.move)
             if board[new_pos[1]][new_pos[0]]:
-                return moves
+                return 
             else:
-                moves.append(new_pos)
-        return moves
+                self.possible_moves.append(new_pos)
